c92b6cea244089f9970d11714177053c15c7a609
Ahamefule Onyegbule <aonyeobi@gmail.com>
2025-03-21 18:08:01 -0400

    Updated project 4

--- server/models.py
+++ server/models.py
@@ -4,3 +4,14 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    password_hash = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    height = db.Column(db.String, nullable=False)
+    weight = db.Column(db.Float, nullable=False)
+    fitness_level = db.Column(db.String, nullable=False)
+    profile_pic = db.Column(db.String, nullable=False)
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,15 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
+        User.query.delete()
+        user1 = User(username="powerlifter20", password_hash="a", email="powerlifter123@email.com", height="5'10\"", weight= 190.0, fitness_level="Beginner", profile_pic="powerlifter.jpeg")
+        db.session.add_all([user1])
+        db.session.commit()
+        print("Seeding successful!")
