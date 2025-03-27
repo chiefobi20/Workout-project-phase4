fab903fa341a256be61f6e59dad8f9eab5f44865
Ahamefule Onyegbule <aonyeobi@gmail.com>
2025-03-27 15:07:48 -0400

    Updated project

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,19 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
+  const [users, setUsers] = useState([])
+  console.log(users)
+
+  useEffect(getUsers, [])
+
+  function getUsers(){
+    fetch('/users')
+    .then((response) => response.json())
+    .then((usersData) => {
+      setUsers(usersData)
+    })
+  }
+
   return <h1>Project Client</h1>;
 }
 
